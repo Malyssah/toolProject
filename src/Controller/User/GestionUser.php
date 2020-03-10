b6587118b8890c1c0aa85a7ac0b1e56946a821f2
Sandrine <didinetbenoit@gmail.com>
2020-03-10 15:57:02 +0100

    users

--- src/Controller/User/GestionUser.php
+++ src/Controller/User/GestionUser.php
@@ -7,7 +7,9 @@ namespace App\Controller\User;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,13 +20,17 @@ class GestionUser extends AbstractController
 
 	/**
 	 * @Route("/user/create",name="create-user")
+	 * @param Request $request
+	 * @param UserPasswordEncoderInterface $encoder
+	 * @return RedirectResponse|Response
 	 */
-	public function createUser(Request $request, UserPasswordEncoderInterface $encoder){
+	public function createUser(Request $request, UserPasswordEncoderInterface $encoder)
+	{
 		$user = new User();
-		$form = $this->createForm(UserType::class,$user);
+		$form = $this->createForm(UserType::class, $user, array('creation' => 1));
 		$form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()){
+		if ($form->isSubmitted() && $form->isValid()) {
 			/** @var User $user */
 			$user = $form->getData();
 			$mdpEncoded = $encoder->encodePassword($user, $user->getPlainPassword());
@@ -33,25 +39,71 @@ class GestionUser extends AbstractController
 			$entitymanager = $this->getDoctrine()->getManager();
 			$entitymanager->persist($user);
 			$entitymanager->flush();
-			$this->addFlash('success','Utilisateur Ajouté !');
+			$this->addFlash('success', 'Utilisateur Ajouté !');
 
 			return $this->redirectToRoute('main');
 		}
 
-		return $this->render('user/add-user.html.twig',array(
-			'form'=>$form->createView(),
+		return $this->render('user/add-user.html.twig', array(
+			'form' => $form->createView(),
 		));
 	}
 
 	/**
 	 * @Route("/user/list", name="list-users")
-	 * @param UserRepository $users
+	 * @param UserRepository $userRepository
 	 * @return Response
 	 */
-	public function usersList(UserRepository $users)
+	public function usersList(UserRepository $userRepository)
 	{
+		$users = $userRepository->findAll();
+
 		return $this->render('user/users.html.twig', [
-			'users' => $users->findAll(),
+			'users' => $users,
 		]);
+	}
+
+	/**
+	 * @Route("/user/edit-user/{id}", name="edit")
+	 * @param User|null $user
+	 * @param Request $request
+	 * @param EntityManagerInterface $manager
+	 * @param UserPasswordEncoderInterface $encoder
+	 * @return RedirectResponse|Response
+	 */
+	public function edit(User $user, Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+	{
+		if (!$user) { //si pas d'utilisateur
+			$user = new User();
+		}
+		$form = $this->createForm(UserType::class, $user, array('creation' => 2));
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			/** @var User $user */
+			//TODO : Faire l'édition avec prise en compte du mdp ou non si null
+			/**
+			 * si mp modifié {
+			 * on encode
+			 * }sinon{
+			 * on ne prend pas en compte
+			 * }
+			 */
+			if ($user === array('creation' => 1)) {
+				$mdpEncoded = $encoder->encodePassword($user, $user->getPlainPassword());
+				$user->setPassword($mdpEncoded);
+				$user->eraseCredentials();
+			} else {
+				$user = $form->getData();
+				dd($user);
+				$manager->persist($user);
+				$manager->flush();
+				$this->addFlash('success', 'Utilisateur Modifié avec succès !');
+				return $this->redirectToRoute('main');
+			}
+		}
+		return $this->render('user/edit.html.twig', array(
+			'form' => $form->createView(),
+		));
 	}
 }
