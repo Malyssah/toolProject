db55945dd1dfa8261bc204320a506443d3bd8cc1
Sandrine <didinetbenoit@gmail.com>
2020-03-11 17:09:56 +0100

    Gestion alliance

--- src/Form/AllianceType.php
+++ src/Form/AllianceType.php
@@ -4,6 +4,7 @@
 namespace App\Form;
 
 
+use App\Entity\Alliance;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
