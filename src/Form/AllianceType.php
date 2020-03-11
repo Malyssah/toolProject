ca5d0322fbb92d2096627e4cf3c3a6e1e2f98616
Sandrine <didinetbenoit@gmail.com>
2020-03-11 15:57:11 +0100

    Gestion alliance

--- src/Form/AllianceType.php
+++ src/Form/AllianceType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AllianceType extends AbstractType
 {
@@ -23,6 +24,12 @@ class AllianceType extends AbstractType
 			))
 			->add('Enregistrer', SubmitType::class);
 
+	}
+
+	public function configureOptions(OptionsResolver $resolver){
+		$resolver->setDefaults([
+			'data_class' => Alliance::class,
+		]);
 	}
 }
 
