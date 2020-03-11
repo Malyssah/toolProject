0723433850103d3cff9ee20dafc7d4d17d36a11a
Sandrine <didinetbenoit@gmail.com>
2020-03-11 10:51:36 +0100

    Gestion User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,28 +13,28 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class User implements UserInterface
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=180, unique=true)
-     */
-    private $email;
+	/**
+	 * @ORM\Column(type="string", length=180, unique=true)
+	 */
+	private $email;
 
-    /**
-     * @ORM\Column(type="json")
-     */
-    private $roles = [];
+	/**
+	 * @ORM\Column(type="json")
+	 */
+	private $roles = [];
 
-    /**
-     * @var string The hashed password
-     * @ORM\Column(type="string")
-     */
-    private $password;
+	/**
+	 * @var string The hashed password
+	 * @ORM\Column(type="string")
+	 */
+	private $password;
 
 	/**
 	 * @param mixed
@@ -43,142 +43,142 @@ class User implements UserInterface
 	 */
 	private $plainPassword;
 
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $username;
+	/**
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 */
+	private $username;
 
-    /**
-     * @ORM\ManyToMany(targetEntity="App\Entity\Alliance", mappedBy="user")
-     */
-    private $alliance;
+	/**
+	 * @ORM\ManyToMany(targetEntity="App\Entity\Alliance", mappedBy="user")
+	 */
+	private $alliance;
 
-    public function __construct()
-    {
-        $this->alliance = new ArrayCollection();
-    }
+	public function __construct()
+	{
+		$this->alliance = new ArrayCollection();
+	}
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+	public function getEmail(): ?string
+	{
+		return $this->email;
+	}
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+	public function setEmail(string $email): self
+	{
+		$this->email = $email;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUsername(): string
-    {
-        return (string) $this->username;
-    }
+	/**
+	 * A visual identifier that represents this user.
+	 *
+	 * @see UserInterface
+	 */
+	public function getUsername(): string
+	{
+		return (string)$this->username;
+	}
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+	/**
+	 * @see UserInterface
+	 */
+	public function getRoles(): array
+	{
+		$roles = $this->roles;
+		// guarantee every user at least has ROLE_USER
+		$roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
-    }
+		return array_unique($roles);
+	}
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
+	public function setRoles(array $roles): self
+	{
+		$this->roles = $roles;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @see UserInterface
-     */
-    public function getPassword(): string
-    {
-        return (string) $this->password;
-    }
+	/**
+	 * @see UserInterface
+	 */
+	public function getPassword(): string
+	{
+		return (string)$this->password;
+	}
 
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
+	public function setPassword(string $password): self
+	{
+		$this->password = $password;
 
-        return $this;
-    }
+		return $this;
+	}
 
 	public function getPlainPassword(): ?string
-                        	{
-                        		return $this->plainPassword;
-                        	}
+	{
+		return $this->plainPassword;
+	}
 
 	public function setPlainPassword(string $plainPassword): self
-                        	{
-                        		$this->plainPassword = $plainPassword;
-                        		return $this;
-                        	}
+	{
+		$this->plainPassword = $plainPassword;
+		return $this;
+	}
 
-    /**
-     * @see UserInterface
-     */
-    public function getSalt()
-    {
-        // not needed when using the "bcrypt" algorithm in security.yaml
-    }
+	/**
+	 * @see UserInterface
+	 */
+	public function getSalt()
+	{
+		// not needed when using the "bcrypt" algorithm in security.yaml
+	}
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials()
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        $this->plainPassword = null;
-    }
+	/**
+	 * @see UserInterface
+	 */
+	public function eraseCredentials()
+	{
+		// If you store any temporary, sensitive data on the user, clear it here
+		$this->plainPassword = null;
+	}
 
-    public function setUsername(?string $username): self
-    {
-        $this->username = $username;
+	public function setUsername(?string $username): self
+	{
+		$this->username = $username;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return Collection|Alliance[]
-     */
-    public function getAlliance(): Collection
-    {
-        return $this->alliance;
-    }
+	/**
+	 * @return Collection|Alliance[]
+	 */
+	public function getAlliance(): Collection
+	{
+		return $this->alliance;
+	}
 
-    public function addAlliance(Alliance $alliance): self
-    {
-        if (!$this->alliance->contains($alliance)) {
-            $this->alliance[] = $alliance;
-            $alliance->addUser($this);
-        }
+	public function addAlliance(Alliance $alliance): self
+	{
+		if (!$this->alliance->contains($alliance)) {
+			$this->alliance[] = $alliance;
+			$alliance->addUser($this);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function removeAlliance(Alliance $alliance): self
-    {
-        if ($this->alliance->contains($alliance)) {
-            $this->alliance->removeElement($alliance);
-            $alliance->removeUser($this);
-        }
+	public function removeAlliance(Alliance $alliance): self
+	{
+		if ($this->alliance->contains($alliance)) {
+			$this->alliance->removeElement($alliance);
+			$alliance->removeUser($this);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 }
